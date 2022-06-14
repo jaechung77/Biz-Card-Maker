6ad13ae2af259dc2d73f749b84ee559c953b9bac
Jae Chung <jaechung77@gmail.com>
2022-06-14 20:57:30 +1000

    added login component

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,12 @@
-import './app.css';
 import Login from '../src/components/login/login';
+import styles from './app.module.css';
 
 function App({ authService }) {
-  console.log(process.env);
-  return <Login authService={authService} />;
+  return (
+    <div className={styles.app}>
+      <Login authService={authService} />
+    </div>
+  );
 }
 
 export default App;
--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
+import styles from './footer.module.css';
 
-const Footer = (props) => <h1>FOoter</h1>; 
+const Footer = (props) => (
+  <footer className={styles.footer}>
+    <p className={styles.title}>Enter Your Name Card</p>
+  </footer>
+);
 
 export default Footer;
